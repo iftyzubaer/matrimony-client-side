939fbe804e1d9e19d763cde16ae084bf2a5fabdb
iftyzubaer <iftyzubaer1304.com>
2023-11-28 01:14:30 +0600

    created the dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,20 @@
+import PropTypes from 'prop-types';
+import { useContext } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+
 const Dashboard = () => {
+
+    const { user } = useContext(AuthContext)
+
     return (
         <div>
-            
+            {user.displayName}
         </div>
     );
 };
 
 export default Dashboard;
+
+Dashboard.propTypes = {
+    user: PropTypes.array,
+}
